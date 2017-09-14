91640b8eb8e112f7a91f684417b18b1ebb8f9f32
disjfa <disjfa@disjfa.nl>
2017-09-14 21:41:28 +0000

    Added favicon, setup eslint

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+require 'json'
+require 'open-uri'
+gem 'github-pages', group: :jekyll_plugins
--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,121 @@
-'use strict'
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const child = require('child_process');
+const gutil = require('gulp-util');
+const ghPages = require('gulp-gh-pages');
+const browserify = require('browserify');
+const vueify = require('vueify');
+const source = require('vinyl-source-stream');
+const favicons = require('gulp-favicons');
 
-let gulp = require('gulp')
-let browserSync = require('browser-sync')
-let sass = require('gulp-sass')
-let child = require('child_process')
-let gutil = require('gulp-util')
-let ghPages = require('gulp-gh-pages')
-let browserify = require('browserify')
-let vueify = require('vueify')
-let source = require('vinyl-source-stream')
+gulp.task('default', ['copy', 'scripts', 'styles', 'jekyll', 'browser-sync', 'watch'], () => {
+});
 
-gulp.task('default', ['copy', 'scripts', 'styles', 'jekyll', 'browser-sync', 'watch'], function () {
-})
+gulp.task('watch', () => {
+  gulp.watch('scss/**/*.scss', ['styles']);
+  gulp.watch('js/**/*.js', ['scripts']);
+});
 
-gulp.task('watch', function () {
-  gulp.watch('scss/**/*.scss', ['styles'])
-  gulp.watch('js/**/*.js', ['scripts'])
-})
+gulp.task('copy', ['copy-fonts'], () => {
 
-gulp.task('copy', ['copy-fonts'], function () {
+});
 
-})
-
-gulp.task('copy-fonts', function () {
+gulp.task('copy-fonts', () => {
   gulp.src('./node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('./site/fonts'))
-})
+    .pipe(gulp.dest('./site/fonts'));
+});
 
-gulp.task('jekyll', function () {
-  let jekyll = child.spawn('jekyll', ['serve',
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['serve',
     '--watch',
     '--drafts',
-    '--config=_config_dev.yml'
-  ])
+    '--config=_config_dev.yml',
+  ]);
 
-  let jekyllLogger = function (buffer) {
+  const jekyllLogger = (buffer) => {
     buffer.toString()
       .split(/\n/)
-      .forEach(function (message) {
-        gutil.log('Jekyll: ' + message)
-      })
-  }
+      .forEach((message) => {
+        gutil.log(`Jekyll: ${message}`);
+      });
+  };
 
-  jekyll.stdout.on('data', jekyllLogger)
-  jekyll.stderr.on('data', jekyllLogger)
-})
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
 
-gulp.task('jekyll-deploy', function () {
-  let jekyll = child.spawn('jekyll', ['build',
-    '--config=_config.yml'
-  ])
+gulp.task('jekyll-deploy', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+    '--config=_config.yml',
+  ]);
 
-  let jekyllLogger = function (buffer) {
+  const jekyllLogger = (buffer) => {
     buffer.toString()
       .split(/\n/)
-      .forEach(function (message) {
-        gutil.log('Jekyll: ' + message)
-      })
-  }
+      .forEach((message) => {
+        gutil.log(`Jekyll: ${message}`);
+      });
+  };
 
-  jekyll.stdout.on('data', jekyllLogger)
-  jekyll.stderr.on('data', jekyllLogger)
-})
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
 
-gulp.task('styles', function () {
+gulp.task('styles', () => {
   gulp.src('scss/css-layout.scss')
     .pipe(sass({
-      includePaths: ['./node_modules/']
+      includePaths: ['./node_modules/'],
     }).on('error', sass.logError))
-    .pipe(gulp.dest('./site/css/'))
+    .pipe(gulp.dest('./site/css/'));
 
   gulp.src('scss/layout.scss')
     .pipe(sass({
-      includePaths: ['./node_modules/']
+      includePaths: ['./node_modules/'],
     }).on('error', sass.logError))
-    .pipe(gulp.dest('./site/css/'))
-})
+    .pipe(gulp.dest('./site/css/'));
+});
 
 // Basic usage
-gulp.task('scripts', function () {
+gulp.task('scripts', () => {
   browserify('./js/happy-thoughts.js')
     .transform(vueify)
     .bundle()
     .pipe(source('happy-thoughts.js'))
-    .pipe(gulp.dest('./site/js'))
-})
+    .pipe(gulp.dest('./site/js'));
+});
 
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', () => {
   browserSync.init(null, {
     files: ['_site/**/*.*'],
     port: 7000,
     server: {
-      baseDir: '_site'
-    }
-  })
-})
+      baseDir: '_site',
+    },
+  });
+});
 
-gulp.task('deploy', [], function () {
-  return gulp.src('./_site/**/*')
-    .pipe(ghPages())
-})
+gulp.task('favicons', () => gulp.src('site/assets/icon.png').pipe(favicons({
+  appName: 'Just a site',
+  appDescription: 'This is an example to build just a site',
+  developerName: 'disjfa',
+  developerURL: 'http://www.disjfa.nl/',
+  background: '#c0392b',
+  path: '/assets/icons/',
+  url: 'https://disjfa.github.io/just-a-site/',
+  display: 'standalone',
+  orientation: 'portrait',
+  start_url: '/?homescreen=1',
+  version: 1.0,
+  logging: false,
+  online: false,
+  html: './../../_includes/meta.html',
+  pipeHTML: true,
+  replace: true,
+}))
+  .on('error', gutil.log)
+  .pipe(gulp.dest('./site/assets/icons/')));
+
+
+gulp.task('deploy', [], () => gulp.src('./_site/**/*')
+  .pipe(ghPages()));
